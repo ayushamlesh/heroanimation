968666001d325647a0a50b873702cd7834cf7245
Ayush Amlesh <43597067+ayushamlesh@users.noreply.github.com>
2022-04-28 01:51:10 +0530

    Update story_start.js

--- story_start.js
+++ story_start.js
@@ -19,7 +19,7 @@ let o = { "duration": 1000 };
 
 //running the animation
 //lightning is objet from top 
-setTimeout(() => {lightning.animate([lightningStart, lightningEnd], options)},100);
+setTimeout(() => {lightning.animate([lightningStart, lightningEnd], options)},1000);
 
 setTimeout(() => { villain.animate([villainStart, villainEnd], o) }, 10000);
 
